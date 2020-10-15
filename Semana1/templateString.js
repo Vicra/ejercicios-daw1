654e1c83ea09ad225e27fc2b23e9903bcabac9b1
Vicra <vicramirez_10@hotmail.com>
2020-10-14 21:02:04 -0600

    temaplte string

---
+++ Semana1/templateString.js
@@ -0,0 +1,30 @@
+let nombre = 'victor'
+let segnombre = 'ricardo'
+let apellido = 'ramirez'
+let apellido2 = 'perdomo'
+
+let nombreCompleto = nombre + ' ' + segnombre + ' ' + apellido + ' ' + apellido2 + ' ' + segnombre + '\n' + apellido + ' ' + apellido2;
+// console.log(nombreCompleto)
+
+let nombreMasiso = 
+    `${nombre} ${segnombre} ${apellido} ${apellido2}`
+console.log(nombreMasiso)
+
+/*
+let insertSQL = `INSERT INTO table
+(
+    name
+    , phone
+    , address
+    , image
+    , url
+)
+VALUES
+(
+    ${name}
+    , ${phone}
+    , ${address}
+    , ${image}
+    , ${url}
+)`;
+*/
