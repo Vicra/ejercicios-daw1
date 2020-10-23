fe28a3439d374cea50a1e3601292e3d7bc90a34a
lufera25 <72897519+lufera25@users.noreply.github.com>
2020-10-22 20:56:20 -0600

    ejercicio Aplicantes

--- Semana2/App.js
+++ Semana2/App.js
@@ -1,6 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json({ extended: true }));
+
+let aplicantes = [
+    {
+        id: 1,
+        nombre:"Luis"
+        ,correo:"lufera25@hotamail.com"
+        ,edad:31
+    },
+    {
+        id: 2,
+        nombre:"German"
+        ,correo:"german@hotamail.com"
+        ,edad:63
+
+    }
+]
 app.get('/', function (req, res) {
-    res.send('Holis');
+    res.send(aplicantes);
+})
+
+app.post('/',(req,res)=>{
+
+  
+    aplicantes.push(req.body);
+    console.log(aplicantes);
+
+    res.sendStatus(200);
+
+})
+app.put('/',(req,res)=>{
+
+  
+    aplicantes.forEach(element => {
+
+        if (element.id == req.body.id ){
+            element.nombre = req.body.nombre ;
+            element.correo = req.body.correo ;
+            element.edad = req.body.edad ;
+        }
+     
+        
+    });
+    console.log(aplicantes);
+    res.sendStatus(200);
+
 })
 app.listen(3000);
